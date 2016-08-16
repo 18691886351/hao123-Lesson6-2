3b24e87c30977ce4cfa6e6dcadb3982a541d3712
qianl <450374167@qq.com>
2016-08-16 15:30:21 +0800

    20160816第二次提交

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,35 @@
 /**点击menu显示或者隐藏**/
 var menu = document.getElementById("search-panel-menu");
 var menu_childs = document.getElementsByClassName("search-panel-menu-tab-child");
-var flag = 1;
-menu.onclick = function() {
-    var menu_tab = document.getElementById("search-panel-menu-tab");
+var menu_tab = document.getElementById("search-panel-menu-tab");
 
-    //alert(menu_childs.length);
-    if (menu_tab.style.display == "none" && flag == 1) {
-        menu_tab.style.display = "block";
-        menu_childs[0].focus();
-        flag = 0;
+/*
+默认的时间触发顺序是foucs>click>blur
+这将click事件优先触发，以避免在点击menu时,先触发foucs,再click而导致的菜单闪烁的情况
+*/
+window.onload = function() {
+    menu.addEventListener('click', menu_click, true);
+    menu.addEventListener('foucs', menu_foucs, true);
+    menu.addEventListener('blur', menu_blur, true);
+}
+
+var menu_click = function() {
+    if (menu_tab.style.display == "block") {
+        menu_tab.style.display = "none"
     } else {
-        menu_childs[0].blur();
-        flag = 1
+        menu_tab.style.display = "block"
     }
 }
 
-menu_childs[0].onblur = function() {
-    var menu_tab = document.getElementById("search-panel-menu-tab");
+
+var menu_foucs = function() {
+    if (menu_tab.style.display == "block") {
+        menu_tab.style.display = "none"
+    } else {
+        menu_tab.style.display = "block"
+    }
+}
+
+var menu_blur = function() {
     menu_tab.style.display = "none"
-    flag = 0;
 }
