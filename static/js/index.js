9192b33ee25936961fe05093b97c0ed0b2fdcd90
qianl <450374167@qq.com>
2016-08-17 20:07:08 +0800

    20160817第二次提交

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,9 @@
 var menu = document.getElementById("search-panel-menu");
 var menu_childs = document.getElementsByClassName("search-panel-menu-tab-child");
 var menu_tab = document.getElementById("search-panel-menu-tab");
-
+var wslider = document.getElementById("wslider");
+var arrow_prev = document.getElementById("arrow-prev");
+var arrow_next = document.getElementById("arrow-next");
 /*
 默认的时间触发顺序是foucs>click>blur
 这将click事件优先触发，以避免在点击menu时,先触发foucs,再click而导致的菜单闪烁的情况
@@ -32,4 +34,15 @@ var menu_foucs = function() {
 
 var menu_blur = function() {
     menu_tab.style.display = "none"
+}
+
+
+wslider.onmouseover = function() {
+    arrow_prev.style.display = "block";
+    arrow_next.style.display = "block";
+}
+
+wslider.onmouseout = function() {
+    arrow_prev.style.display = "none";
+    arrow_next.style.display = "none";
 }
